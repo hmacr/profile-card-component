6316aee940b4a4159e7f924824f5fec563f8e536
hmacr <hmac.devo@gmail.com>
2023-09-08 10:13:12 +0530

    setup tailwindcss colors, typography, and bg-img

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,22 @@
+import defaultTheme from 'tailwindcss/defaultTheme';
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html'],
-  theme: {},
+  theme: {
+    colors: {
+      'dark-cyan': 'hsl(185, 75%, 39%)',
+      'dark-desaturated-blue': 'hsl(229, 23%, 23%)',
+      'dark-grayish-blue': 'hsl(227, 10%, 46%)',
+      'dark-gray': 'hsl(0, 0%, 59%)',
+    },
+    fontFamily: {
+      'kumbh-sans': ['Kumbh Sans', ...defaultTheme.fontFamily.sans],
+    },
+    backgroundImage: {
+      'pattern-card': 'url("./images/bg-pattern-card.svg")',
+      'pattern-top': 'url("./images/bg-pattern-top.svg")',
+      'pattern-bottom': 'url("./images/bg-pattern-bottom.svg")',
+    },
+  },
 };
